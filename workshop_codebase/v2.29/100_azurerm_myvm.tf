ab4fce48f61763d91249eb30ee6c2fe39f0776c7
Hank Wojteczko <hankwojteczko@davidkentconsulting.com>
2020-10-01 17:12:11 -0500

    working an issue

--- workshop_codebase/v2.29/100_azurerm_myvm.tf
+++ workshop_codebase/v2.29/100_azurerm_myvm.tf
@@ -1,73 +1,73 @@
-resource "azurerm_public_ip" "DKCESMT01_pubip" {
-    name                = var.azurevm_DKCESMT01.vm_pubip_name
-    location            = var.azure_region
-    resource_group_name = azurerm_resource_group.myResourceGroup.name
-    sku                 = "Basic"
-    allocation_method   = var.azurevm_DKCESMT01.vm_pubip_alloc_method
+# resource "azurerm_public_ip" "DKCESMT01_pubip" {
+#     name                = var.azurevm_DKCESMT01.vm_pubip_name
+#     location            = var.azure_region
+#     resource_group_name = azurerm_resource_group.myResourceGroup.name
+#     sku                 = "Basic"
+#     allocation_method   = var.azurevm_DKCESMT01.vm_pubip_alloc_method
 
-    tags = {
-        accounting  = var.accounting_tag
-    }
-}
+#     tags = {
+#         accounting  = var.accounting_tag
+#     }
+# }
 
-resource "azurerm_network_interface" "DKCESMT01_nic01" {
-    name                = var.azurevm_DKCESMT01.vm_nicname
-    location            = var.azure_region
-    resource_group_name = azurerm_resource_group.myResourceGroup.name
-    ip_configuration {
-        name                            = "ipconfig1"
-        subnet_id                       = azurerm_subnet.my_subnet0.id
-        private_ip_address_allocation   = var.azurevm_DKCESMT01.vm_nicip_alloc_method
-    }
+# resource "azurerm_network_interface" "DKCESMT01_nic01" {
+#     name                = var.azurevm_DKCESMT01.vm_nicname
+#     location            = var.azure_region
+#     resource_group_name = azurerm_resource_group.myResourceGroup.name
+#     ip_configuration {
+#         name                            = "ipconfig1"
+#         subnet_id                       = azurerm_subnet.my_subnet0.id
+#         private_ip_address_allocation   = var.azurevm_DKCESMT01.vm_nicip_alloc_method
+#     }
 
-    tags = {
-        accounting  = var.accounting_tag
-    }
-}
-
-
-resource "azurerm_linux_virtual_machine" "azurevm_DKCESMT01" {
-    name                            = var.azurevm_DKCESMT01.vm_name
-    location                        = var.azure_region
-    resource_group_name             = azurerm_resource_group.myResourceGroup.name
-    size                            = var.azurevm_DKCESMT01.size
-    admin_username                  = var.azurevm_DKCESMT01.username
-    admin_password                  = var.azurevm_DKCESMT01.admin_password
-    disable_password_authentication = false
-    network_interface_ids = [
-        azurerm_network_interface.DKCESMT01_nic01.id,
-    ]
+#     tags = {
+#         accounting  = var.accounting_tag
+#     }
+# }
 
 
-    # admin_ssh_key {
-    #     username            = var.azurevm_DKCESMT01.username
-    #     public_key          = file("~/.ssh/id_rsa.pub")
-    # }
+# resource "azurerm_linux_virtual_machine" "azurevm_DKCESMT01" {
+#     name                            = var.azurevm_DKCESMT01.vm_name
+#     location                        = var.azure_region
+#     resource_group_name             = azurerm_resource_group.myResourceGroup.name
+#     size                            = var.azurevm_DKCESMT01.size
+#     admin_username                  = var.azurevm_DKCESMT01.username
+#     admin_password                  = var.azurevm_DKCESMT01.admin_password
+#     disable_password_authentication = false
+#     network_interface_ids = [
+#         azurerm_network_interface.DKCESMT01_nic01.id,
+#     ]
 
-    os_disk {
-        name                        = var.azurevm_DKCESMT01.os_disk_name
-        disk_size_gb                = var.azurevm_DKCESMT01.os_disk_size_gb
-        caching                     = var.azurevm_DKCESMT01.os_disk_caching
-        storage_account_type        = var.azurevm_DKCESMT01.os_storage_act_type
-    }
+
+#     # admin_ssh_key {
+#     #     username            = var.azurevm_DKCESMT01.username
+#     #     public_key          = file("~/.ssh/id_rsa.pub")
+#     # }
+
+#     os_disk {
+#         name                        = var.azurevm_DKCESMT01.os_disk_name
+#         disk_size_gb                = var.azurevm_DKCESMT01.os_disk_size_gb
+#         caching                     = var.azurevm_DKCESMT01.os_disk_caching
+#         storage_account_type        = var.azurevm_DKCESMT01.os_storage_act_type
+#     }
     
-    boot_diagnostics {
-        storage_account_uri         = azurerm_storage_account.my_storage_account.primary_blob_endpoint
-    }
+#     boot_diagnostics {
+#         storage_account_uri         = azurerm_storage_account.my_storage_account.primary_blob_endpoint
+#     }
 
 
-    source_image_reference {
-      publisher                     = var.azurevm_DKCESMT01.publisher
-      offer                         = var.azurevm_DKCESMT01.offer
-      sku                           = var.azurevm_DKCESMT01.sku
-      version                       = "latest"
-    }
+#     source_image_reference {
+#       publisher                     = var.azurevm_DKCESMT01.publisher
+#       offer                         = var.azurevm_DKCESMT01.offer
+#       sku                           = var.azurevm_DKCESMT01.sku
+#       version                       = "latest"
+#     }
 
 
-    tags = {
-        accounting  = var.accounting_tag
-    }
-}
+#     tags = {
+#         accounting  = var.accounting_tag
+#     }
+# }
 
 // In the following code blocks, we build instances of identical sizes by using the
 // for_each function. The key concept to get is that the foreach iterative loop
